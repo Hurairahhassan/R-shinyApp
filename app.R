589eaa77aaeae07bb30e67a77bbe794d7de59d57
Syed Hurairah Hassan <110546397+Hurairahhassan@users.noreply.github.com>
2024-02-02 19:10:01 +0500

    Add files via upload

--- app.R
+++ app.R
@@ -1,133 +1,145 @@
-library(bslib)
-library(geojsonio)
-library(leaflet)
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
-library(shinyWidgets)
-library(sp)
+library(tidyr)
+library(dbplyr)
+library(tidyverse)
 
-load(file = "projectData.rdata") #loads full.df and states (see documentation for details)
+avocado <- read.csv("avocado2.csv")
 
-sex <- df$Gender
-race <- df$Race_Ethnicity
+avocado <- avocado %>%
+  select(-X) %>%
+  pivot_longer(cols = starts_with("X4"), names_to = "avo_size", values_to = "avo_volumn")
 
-# Define UI for application
+print(avocado)
+
+plot_epicurve <- function(data, type = "All", avocadosize = "X400") {
+  
+  if (!("All" %in% type)) {
+    data <- data %>%
+      filter(Type %in% type)
+    
+    plot_title_type <- stringr::str_glue("{paste0(type, collapse = ', ')} types")
+    
+  } else {
+    
+    plot_title_type <- "all types"
+    
+  }
+  
+  # if no remaining data, return NULL
+  if (nrow(data) == 0) {
+    
+    return(NULL)
+  }
+  
+  data <- data %>%
+    filter(avo_size == avocadosize)
+  
+  
+  # if no remaining data, return NULL
+  if (nrow(data) == 0) {
+    
+    return(NULL)
+  }
+  
+  if (avocadosize == "X400") {
+    avocadosize_title <- "All sizes"
+  } else {
+    avocadosize_title <- stringr::str_glue("{str_remove(avocadosize, 'X')}")
+  }
+  
+  
+  ggplot(data, aes(x = Date, y = avo_volumn)) +
+    geom_col(width = 0.6, fill = "darkolivegreen4") + 
+    scale_x_discrete(guide = guide_axis(check.overlap = TRUE))+
+    theme_minimal() +
+    labs(
+      x = "Date",
+      y = "Number of avos sales",
+      title = stringr::str_glue("Hass avocado - {plot_title_type}"),
+      subtitle = avocadosize_title
+    )
+  
+  
+  
+}
+# Define UI for application that draws a histogram
 ui <- fluidPage(
-    theme = bs_theme(version = 4, bootswatch = "minty"),
+  
+  titlePanel("Avocado Sales Visualisation"),
+  
+  sidebarLayout(
     
-    
-    titlePanel(
-        "Heart Disease Mortality Data Among US Adults (35+) by State – 2016-2018"
+    sidebarPanel(
+      # selector for avocado type
+      selectInput(
+        inputId = "select_avocadotype",
+        label = "Select Avocado Type",
+        choices = c(
+          "All",
+          "Coventional"="conventional",
+          "Organic"="organic"
+        ),
+        selected = "All",
+        multiple = TRUE
+      ),
+      # selector for avocado size group
+      selectInput(
+        inputId = "select_avocadosize",
+        label = "Select Avocado Size",
+        choices = c(
+          "All" = "X400",
+          "Small/ Medium" = "X4046",
+          "Large" = "X4225",
+          "Extra Large" = "X4770"
+        ), 
+        selected = "All",
+        multiple = FALSE
+      ),
+      # horizontal line
+      hr(),
+      downloadButton(
+        outputId = "download_epicurve",
+        label = "Download plot"
+      )
+      
     ),
     
-    sidebarLayout(
-        sidebarPanel(
-            checkboxGroupInput(
-                "sexInput",
-                "Display values by sex",
-                choices = unique(sex),
-                # choices = c("male","female"),
-                selected = unique(sex)
-                # selected = "male"
-            ),
-            checkboxGroupInput(
-                "raceInput",
-                "Display values by race",
-                choices = unique(race),
-                selected = unique(race)
-            )
-        ),
-        mainPanel(tabsetPanel(
-            type = "tabs",
-            tabPanel("Map", leafletOutput("map")),
-            tabPanel("Data", tableOutput("table"))
-        ))
+    mainPanel(
+      # epicurve goes here
+      plotOutput("avocado_epicurve"),
+      br(),
+      hr(),
+      p("Hey avo lovers! Welcome to the avocado sales visualisation app! To use this app, manipulate the widgets on the side to change the curve according to your preferences! To download a high quality image of the plot you've created, you can also download it with the download button. To see the raw data, use the raw data tab for an interactive form of the table. The data dictionary is as follows:"),
+      tags$ul(
+        tags$li(tags$b("region"), " - the city or region that the data were collected at"),
+        tags$li(tags$b("date"), " - the date the data were collected at"),
+        tags$li(tags$b("X4046"), " - small/ medium avocado (3-5oz)"),
+        tags$li(tags$b("X4225"), " - large avocado (8-10oz)"),
+        tags$li(tags$b("X4770"), " - extra large avocado (10-15oz)"),
+        tags$li(tags$b("X400"), " - total volume of avocado sale"),
+        tags$li(tags$b("type"), " - organic or conventional avocado")
+      )
+      
     )
+  )
 )
-
-# Define server logic
-server <- function(input, output) {
-    US_State_data <- reactive({
-        df = subset(
-            full.df,
-            full.df$Gender %in% input$sexInput &
-                full.df$Race_Ethnicity %in% input$raceInput
-        )
-        
-        DataLookup <-
-            aggregate(df$Data_Value, by = list(df$LocationDesc), sum)
-        colnames(DataLookup) = c("State", "Data_Value")
-        
-        data_values <- DataLookup$Data_Value
-        state_names <- DataLookup$State
-        names(data_values) <- state_names
-        
-        
-        US_States <-
-            states[states$NAME %in% full.df$LocationDesc,]
-        US_States$DataValue <- data_values[US_States$NAME]
-        
-        
-        return(US_States)
-    })
-    
-    labels_data <- reactive({
-        labels <- sprintf(
-            "<strong>%s</strong><br/>%g deaths per 100,000 population</sup>",
-            US_State_data()$NAME,
-            US_State_data()$DataValue
-        ) %>% lapply(htmltools::HTML)
-        return(labels)
-    })
-    
-    pal_data <- reactive({
-        # bins <- c(0, 200, 300, 400, 500, 1000, 2000, 3000, 4000, 5000)
-        pal <-
-            colorNumeric("YlOrRd",
-                         domain = US_State_data()$DataValue)
-        return(pal)
-    })
-    
-    output$table <- renderTable({
-        tab <- US_State_data()
-        out <- tab[, c("NAME", "DataValue")]
-        out = as.data.frame(out)
-        colnames(out) = c("State",
-                          "Deaths by Heart Disease (per 100,000 population) ")
-        out
-    })
-    
-    output$map <- renderLeaflet({
-        leaflet(US_State_data()) %>%
-            setView(-96, 37.8, 4) %>% addPolygons(
-                fillColor = ~ pal_data()(DataValue),
-                weight = 2,
-                opacity = 1,
-                color = "white",
-                dashArray = "3",
-                fillOpacity = 0.7,
-                highlight = highlightOptions(
-                    weight = 5,
-                    color = "#666",
-                    dashArray = "",
-                    fillOpacity = 0.7,
-                    bringToFront = TRUE
-                ),
-                label = labels_data(),
-                labelOptions = labelOptions(
-                    style = list("font-weight" = "normal", padding = "3px 8px"),
-                    textsize = "15px",
-                    direction = "auto"
-                )
-            ) %>%
-            addLegend(
-                pal = pal_data(),
-                values = ~ DataValue,
-                opacity = 0.7,
-                title = NULL,
-                position = "bottomright"
-            )
-    })
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  
+  output$avocado_epicurve <- renderPlot(
+    plot_epicurve(avocado, type = input$select_avocadotype, avocadosize = input$select_avocadosize)
+  )
+  
 }
 
-# Run the application
+# Run the application 
 shinyApp(ui = ui, server = server)
